8812fd356e57cb43dfa9e81dc0fcfe98590c9fce
Clem <valles.clement@gmail.com>
2021-02-27 15:46:58 -0600

    first components and structure of the portfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
+import React from 'react';
 import logo from './logo.svg';
+import { BrowserRouter, Route, Link } from 'react-router-dom'; 
+
+import Work from './components/Work'; 
+import About from './components/About';  
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+
+        {/* Set up the Router */}
+        <Route exact path="/" component={Work} />
+        <Route exact path="/about" component={About} />
+        
+        <div className="Navigation">
+              <img src={logo} className="Logo" alt="Clem portfolio logo" />
+
+              <div className="Navigation-sub">
+                {/* Set up the links */}
+                <Link href='' className="Sub-item">Work</Link>
+                <Link href='' className="Sub-item">About Me</Link>
+              </div>
+          </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
