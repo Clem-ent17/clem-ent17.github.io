b9c433291becc42ae203612f29afe721da06c537
Clem <valles.clement@gmail.com>
2021-03-01 18:13:34 -0600

    Fied responsive display on About page

--- src/components/About.js
+++ src/components/About.js
@@ -24,23 +24,26 @@ function About(props) {
   return (
     <div className={classes.root}>
       <Grid container spacing={4}>
-        <Grid item xs={12}>
+        <Grid item xs={12} >
           
         </Grid>
-        <Grid item xs={6} sm={6}>
+        <Grid item xs={12} sm={6}>
           <h1>Hi, I am<br/><span className="gold">Clem</span>ent Valles</h1>
 
           <p className="IntroText">I’m passionate about two things: solving problems, even sometimes before they exist, and making a difference with my design.</p>
 
           <p className="IntroText">I’m user focused. I always try to create human and intuitive design allowing the users to understand its value in just a few seconds. This is the power of design.</p>
         </Grid>
-        <Grid item xs={6} sm={6}>
+        <Grid item xs={12} sm={6}>
           <img src={image} alt="About me Clement Valles" class="img-portfolio img-shadow"/>
         </Grid>
-        <Grid item xs={6} sm={6}>
+        <Grid item xs={12} sm={2}>
+          
+        </Grid>
+        <Grid item xs={12} sm={4}>
           <h1>A bit more<br/>about who<br/>I am</h1>
         </Grid>
-        <Grid item xs={6} sm={6}>
+        <Grid item xs={12} sm={5}>
           <p>Originally from France, I moved to Chicago in 2018 as a permanent resident. I wish to utilize my professional experiences as well as my passion for design, art, and new technologies in a dynamic and innovative career setting.</p>
 
           <p>One of my defining qualities as a designer and a coder is my ability to take a step back from my work and to look at it through the eyes of the user. Throughout my many professional experiences, teamwork has always been one of my core values. I believe that together, as a team, nothing is impossible.</p>
